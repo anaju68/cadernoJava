38e644859111720ee06c312de23b290cae7fe0f4
anaju68 <129894377+anaju68@users.noreply.github.com>
2023-06-06 17:43:38 -0300

    Cvariaveisbooleana.kt

---
+++ variaveisbooleana.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+  //criar variavel do tipo boolenana(false true)
+ var nomeVariavel: Boolean = false 
+  println(nomeVariavel)
+  var x: Boolean = false 
+  println(x)
+  //if faz teste e retorna boolean 
+
+  if (x) {
+    print("verdadeiro")
+  }
+  else{
+    println("falso")
+  }
+  
+}
